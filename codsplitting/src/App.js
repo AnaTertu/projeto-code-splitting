602e1bcf6bb57cb769a5fc69cb2f3ea80e9d34e0
Ana Tertuliano <ana.tertu.2019@gmail.com>
2021-02-25 08:54:49 -0300

    Lazyloading (#2)
    * realizado code splitting no Slogan e Rodape
    
    * aplicado lazy loading

--- codsplitting/src/App.js
+++ codsplitting/src/App.js
@@ -1,19 +1,29 @@
+import React from 'react';
+import {lazy, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Slogan from './components/Slogan';
-import Rodape from './components/Rodape';
+//import Slogan from './components/Slogan';
+//import Rodape from './components/Rodape';
+
+const Slogan = React.lazy(() => import('./components/Slogan'));
+const Rodape = React.lazy(() => import('./components/Rodape'));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Slogan/>
-        <Rodape/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <Suspense fallback={ <p>Carregando...</p> }>
+                    <Slogan/>
+                </Suspense>
+                <Suspense fallback={ <p>Carregando...</p> }>
+                    <Rodape/>
+                </Suspense> 
+            </header>  
+        </div>      
+    );
 }
 
 export default App;
